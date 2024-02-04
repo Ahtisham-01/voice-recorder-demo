a9b48b87097afdd3a5f0d5759c0d5251a54cd6ea
Ahtisham kiani <ehtisham.alphasquad@gmail.com>
2024-02-04 19:43:50 +0500

    first commit

---
+++ src/app/components/VoiceRecorder/index.tsx
@@ -0,0 +1,163 @@
+"use client";
+
+import { useEffect, useState, useRef } from "react";
+
+// Declare a global interface to add the webkitSpeechRecognition property to the Window object
+declare global {
+  interface Window {
+    webkitSpeechRecognition: any;
+  }
+}
+
+const VoiceRecording: React.FC = () => {
+  const [isRecording, setIsRecording] = useState<boolean>(false);
+  const [recordingComplete, setRecordingComplete] = useState<boolean>(false);
+  const [transcript, setTranscript] = useState<string>("");
+  const [audioUrl, setAudioUrl] = useState<string>("");
+  const recognitionRef = useRef<any | null>(null);
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+  const audioChunksRef = useRef<Blob[]>([]);
+
+  const startRecording = () => {
+    setIsRecording(true);
+    setAudioUrl("");
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then((stream) => {
+        mediaRecorderRef.current = new MediaRecorder(stream);
+        mediaRecorderRef.current.start();
+
+        mediaRecorderRef.current.ondataavailable = (event) => {
+          audioChunksRef.current.push(event.data);
+        };
+
+        recognitionRef.current = new window.webkitSpeechRecognition();
+        recognitionRef.current.continuous = true;
+        recognitionRef.current.interimResults = true;
+        recognitionRef.current.onresult = (event: any) => {
+          const { transcript } = event.results[event.results.length - 1][0];
+          console.log(event.results);
+          setTranscript(transcript);
+        };
+        recognitionRef.current.start();
+      })
+      .catch((error) => console.error("Error accessing the microphone", error));
+  };
+
+  const stopRecording = () => {
+    if (recognitionRef.current) {
+      recognitionRef.current.stop();
+    }
+    if (mediaRecorderRef.current) {
+      mediaRecorderRef.current.stop();
+      mediaRecorderRef.current.onstop = () => {
+        const audioBlob = new Blob(audioChunksRef.current, {
+          type: "audio/wav",
+        });
+        const audioUrl = URL.createObjectURL(audioBlob);
+        setAudioUrl(audioUrl);
+        setRecordingComplete(true);
+        // Reset the audio chunks array for the next recording
+        audioChunksRef.current = [];
+      };
+    }
+  };
+
+  const handleToggleRecording = () => {
+    if (!isRecording) {
+      startRecording();
+    } else {
+      stopRecording();
+    }
+    setIsRecording(!isRecording);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (
+        mediaRecorderRef.current &&
+        mediaRecorderRef.current.state !== "inactive"
+      ) {
+        mediaRecorderRef.current.stop();
+      }
+    };
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center px-4 bg-gray-800  justify-center h-screen w-full">
+      <div className="w-full">
+        {(isRecording || transcript) && (
+          <div className="w-wull  md:w-1/4 m-auto rounded-md border p-4 bg-white">
+
+            <div className="flex-1 flex w-full justify-between">
+              <div className="space-y-1">
+                <p className="text-sm font-medium leading-none">
+                  {recordingComplete ? "Recorded" : "Recording"}
+                </p>
+                <p className="text-sm text-muted-foreground">
+                  {recordingComplete
+                    ? "Thanks for talking."
+                    : "Start speaking..."}
+                </p>
+              </div>
+              {isRecording && (
+                <div className="rounded-full w-4 h-4 bg-red-400 animate-pulse" />
+              )}
+            </div>
+
+            {transcript && (
+              <div className="border rounded-md p-2 h-fullm mt-4">
+                <p className="mb-0">{transcript}</p>
+              </div>
+            )}
+          </div>
+        )}
+
+        <div className="flex items-center w-full">
+          {isRecording ? (
+            // Button for stopping recording
+            <button
+              onClick={handleToggleRecording}
+              className="mt-10 m-auto flex items-center justify-center bg-red-400 hover:bg-red-500 rounded-full w-20 h-20 focus:outline-none"
+            >
+              <svg
+                className="h-12 w-12 "
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path fill="white" d="M6 19h4V5H6v14zm8-14v14h4V5h-4z" />
+              </svg>
+            </button>
+          ) : (
+            // Button for starting recording
+            <button
+              onClick={handleToggleRecording}
+              className="mt-10 m-auto flex items-center justify-center bg-blue-400 hover:bg-blue-500 rounded-full w-20 h-20 focus:outline-none"
+            >
+              <svg
+                viewBox="0 0 256 256"
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-12 h-12 text-white"
+              >
+                <path
+                  fill="currentColor"
+                  d="M128 176a48.05 48.05 0 0 0 48-48V64a48 48 0 0 0-96 0v64a48.05 48.05 0 0 0 48 48ZM96 64a32 32 0 0 1 64 0v64a32 32 0 0 1-64 0Zm40 143.6V232a8 8 0 0 1-16 0v-24.4A80.11 80.11 0 0 1 48 128a8 8 0 0 1 16 0a64 64 0 0 0 128 0a8 8 0 0 1 16 0a80.11 80.11 0 0 1-72 79.6Z"
+                />
+              </svg>
+            </button>
+          )}
+        </div>
+        {audioUrl && (
+          <div className="w-wull px-4 md:w-1/4  m-auto rounded-md border mt-4 p-4 bg-white">
+            <p className="mb-2">Recorded Audio:</p>
+            <audio className="w-full" controls>
+              <source src={audioUrl} type="audio/wav" />
+              Your browser does not support the audio element.
+            </audio>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+export default VoiceRecording;
